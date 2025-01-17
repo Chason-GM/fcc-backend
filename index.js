7bfc97185266428831edd6c5ebfcd13b11892e72
Check <check@Checks-MacBook-Pro.local>
2025-01-18 04:58:34 +0700

    feat: multer

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 require('dotenv').config()
 
 var app = express();
@@ -11,6 +13,10 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+
+  res.json({name: req.file.originalname, type : req.file.mimetype, size: req.file.size})
+})
 
 
 
